5313276c0e15bc4b8f94244b1468c496ab03716c
TheGhost <theghostgroup@gmail.com>
2025-08-02 16:14:50 +1200

    Core/AsyncAcceptor: fix compile warning due to unsigned int

--- src/server/shared/Networking/AsyncAcceptor.h
+++ src/server/shared/Networking/AsyncAcceptor.h
@@ -140,7 +140,7 @@ void AsyncAcceptor::AsyncAccept()
             }
             catch (boost::system::system_error const& err)
             {
-                TC_LOG_INFO("network", "Failed to retrieve client's remote address %u", err.what());
+                TC_LOG_INFO("network", "Failed to retrieve client's remote address %s", err.what());
             }
         }
 
